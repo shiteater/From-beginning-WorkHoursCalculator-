1db4f61ace097bea3be7376017b3faa8b10919dd
shiteater <kojetajpokemon@yahoo.co.uk>
2017-07-30 18:20:25 +0200

    Krećem ispočetka

---
+++ From beginning/Basic.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Basic.aspx.cs" Inherits="From_beginning.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+   <asp:TextBox ID="TbxWorkedHouresOnThisDay" runat="server" Width="180px">Worked Houres On This Day</asp:TextBox>
+    <asp:Calendar ID="Calendar1" runat="server" style="top: 23px; left: 1100px; float: right; height: 188px; width: 513px;" SelectedDate="<%# DateTime.Today %>" >
+        <SelectedDayStyle BackColor="#6699FF" />
+        <TodayDayStyle BackColor="#FFFFCC" />
+    </asp:Calendar>
+    <br />
+    <br />
+    <br />
+    <asp:TextBox ID="TbxHourPrice" runat="server" Width="180px">Hour price</asp:TextBox>
+    <br />
+    <br />
+    Calculation for<asp:DropDownList ID="ddlMode" runat="server">
+        <asp:ListItem>1 day</asp:ListItem>
+        <asp:ListItem>1 week</asp:ListItem>
+        <asp:ListItem>1 month</asp:ListItem>
+        <asp:ListItem>a year</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:TextBox ID="TbxTotalHours" runat="server" Width="180px">Total Hours</asp:TextBox>
+    &nbsp;<br />
+    <br />
+    <br />
+    <asp:TextBox ID="TbxTotalEarnings" runat="server" Width="180px">Total Earnings</asp:TextBox>
+    <br />
+    <br />
+    <br />
+    <asp:Button ID="BtnSaveChanges" runat="server" Text="SaveChanges" OnClick="BtnSaveChanges_Click" />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+</asp:Content>
---
+++ From beginning/Basic.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace From_beginning
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnSaveChanges_Click(object sender, EventArgs e)
+        {
+            //promijenite con
+
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@V1", Convert.ToDateTime(Calendar1.SelectedDate));
+            //trenutno ovo '1' je id korisnika koji trebamo povezati sa loginom
+            cmd.CommandText = $"insert into dbo.Kalkulacije values( '1' , @V1, Null, Null,'{TbxWorkedHouresOnThisDay.Text}', '{TbxHourPrice.Text}')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Write("<script>alert('Fak yea it is saved :D');</script>");
+            con.Close();
+        }
+        //upisivanje nema problema
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-K1I0JMC\SQLEXPRESS;Initial Catalog=WorkHours;Integrated Security=True;Pooling=False");
+
+            // vađenje podataka iz sql i upisivanje u textboxeve
+
+
+            string selectSql = "select * from Kalkulacije where Ukupno_odradeno_sati";
+            SqlCommand cmd = new SqlCommand(selectSql, con);
+
+            try
+            {
+                con.Open();
+
+                using (SqlDataReader read = cmd.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        TbxTotalHours.Text = (read["Ukupno_odradeno_sati"].ToString());
+
+                    }
+                }
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        
+    }
+}
